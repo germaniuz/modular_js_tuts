960893f4890e2da24a8cc36ef55b7b8b19353c28
germaniuz <germanius@list.ru>
2017-05-01 23:00:37 +0300

    End of first video

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,14 @@
-console.log('hello!');
+var myModule = {
+  name: 'German',
+  age: 31,
+  sayName: function(){
+    console.log(this.name);
+  },
+  setName: function(newName){
+    this.name = newName;
+  }
+};
+
+myModule.sayName();
+myModule.setName("Sofia");
+myModule.sayName();
